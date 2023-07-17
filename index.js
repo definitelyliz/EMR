59080eb73493a5e498c8cef961e0df2c5b7e05e2
foodequalslife2 <114330898+foodequalslife2@users.noreply.github.com>
2023-07-17 23:16:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
